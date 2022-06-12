c5692bbfb10d72dd4b6adcd580b0711bb37578ff
andlekoal <andlekoal@gmail.com>
2022-06-12 12:59:00 +0300

    Start, stop, reset buttons functionality for all stopwatches added

--- app/src/main/java/com/example/geekbrainsstopwatch/view/MainActivity.kt
+++ app/src/main/java/com/example/geekbrainsstopwatch/view/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.geekbrainsstopwatch.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.geekbrainsstopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
